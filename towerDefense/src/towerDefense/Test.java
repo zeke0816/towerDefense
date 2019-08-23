adfb86bd5ecea2ba907b387d032036af71cafc63
dimax <7unix7@gmail.com>
2019-08-23 15:03:02 -0300

    Agregue la clase Test, para testear que nos funcione a todos.

---
+++ towerDefense/src/towerDefense/Test.java
@@ -0,0 +1,18 @@
+package towerDefense;
+
+public class Test {
+
+	/*
+	 * Vane genera e imprime b
+	 * Zeke genera e imprime c
+	 * Todos quedamos con a,b,c creadas.
+	 * GG 
+	 */
+	public static void main(String[] args) {
+		int a = 5;
+		
+		System.out.println(a);
+		
+	}
+
+}
