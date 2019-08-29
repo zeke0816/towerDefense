2e84d803a1a95dfad741d4ef23b8d85303314963
zeke0816 <>
2019-08-29 20:40:51 -0300

    Testing Git on Eclipse.

--- towerDefense/src/towerDefense/Test.java
+++ towerDefense/src/towerDefense/Test.java
@@ -6,13 +6,16 @@ public class Test {
 	 * Vane genera e imprime b
 	 * Zeke genera e imprime c
 	 * Todos quedamos con a,b,c creadas.
-	 * GG .
+	 * GG.
 	 */
 	public static void main(String[] args) {
 		int a = 5;
+		int b = 12;
+		int c = 47;
 		
 		System.out.println(a);
-		
+		System.out.println(b);
+		System.out.println(c);
 	}
 
 }
