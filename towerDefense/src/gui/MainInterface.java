cca559bf09122143d39901d8b24d78d18e6a516e
Zeke <zeke0816@VAIO>
2019-10-04 22:41:04 -0300

    Oh, well...

--- towerDefense/src/gui/MainInterface.java
+++ towerDefense/src/gui/MainInterface.java
@@ -33,6 +33,8 @@ public class MainInterface extends Application {
      */
 	@Override
 	public void start(Stage stage) throws Exception {
+		System.out.println("Dmytro, your PC is the problem.");
+		
 		stage.setTitle("Cartoon Defense");
 		stage.centerOnScreen();
 		stage.setWidth(1500);
