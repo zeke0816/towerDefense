948f708b3b7a29006520997d37a4dba4dfea6a98
unix77 <7unix7@gmail.com>
2019-11-07 18:45:02 -0300

    Jiren

---
+++ towerDefense/src/game/objects/characters/enemies/Jiren.java
@@ -0,0 +1,25 @@
+package game.objects.characters.enemies;
+
+public class Jiren extends Enemy {
+
+	public Jiren() {
+		LIFE = 2000;
+		scope = 10;
+		life = LIFE;
+		points = 1000;
+		STRENGTH = 1000;
+		movementSpeed = 5;
+		life = LIFE;
+		strength = STRENGTH;
+	}
+	
+	private Jiren(Jiren target) {
+		super(target);
+	}
+	
+	@Override
+	public Enemy clone() {
+		return new Jiren(this);
+	}
+
+}
